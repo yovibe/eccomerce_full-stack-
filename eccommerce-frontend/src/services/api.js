2c3a4a71cec367bce5e227d43f5f2dfc96e69c6e
Yordanos <abayjordan540@gmail.com>
2026-04-01 10:47:52 -0400

    fixed one to cross

--- eccommerce-frontend/src/services/api.js
+++ eccommerce-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://eccomerce-full-stack.onrender.com/api',
 });
 
 // Automatically attach JWT token to all requests
